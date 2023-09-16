539294772d80de83bdac718551ae2635df4d5d06
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-16 14:31:04 +0500

    header uchun component berib yubordim.

---
+++ src/components/header/header.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import "./header.css";
+
+export const Header = () => {
+  return (
+    <div className="Header">
+      <div className="Header__floor1">
+        <h1 className="Header__logo">T. &nbsp;Market</h1>
+        <div className="Header__nav">
+          <Link to="/" className="Header__link">
+            <i className="bx bx-home-alt icon"></i>
+            Asosiy
+          </Link>
+          <Link to="/" className="Header__link">
+            <i className="bx bx-store icon"></i>
+            Do'konlar
+          </Link>
+          <Link to="/" className="Header__link">
+            <i className="bx bx-news icon"></i>
+            Maqolalar
+          </Link>
+          <Link to="/" className="Header__link">
+            <i className="bx bx-user-plus icon"></i>
+            Ro'yxatdan o'tish
+          </Link>
+          <Link to="/" className="Header__link">
+            <i className="bx bx-log-in icon"></i>
+            Kirish
+          </Link>
+        </div>
+      </div>
+      <div className="Header__floor2"></div>
+    </div>
+  );
+};
