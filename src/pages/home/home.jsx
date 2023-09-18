2055c6d6ce0e68ef81d18d2e813997b150d3ac11
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-18 17:13:56 +0500

    Products ochildi.

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
 import "./home.css";
-import { Header, HomeMain } from "../../components";
+import { Header, HomeMain, Products } from "../../components";
 export const Home = () => {
   return (
     <Fragment>
       <Header />
       <HomeMain />
+      <Products />
     </Fragment>
   );
 };
