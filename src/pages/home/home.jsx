bb91563d5b83b3c5982632f21a22092fde151d48
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-18 16:55:44 +0500

    homeda home mainni olib kelindi

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
 import "./home.css";
-import { Header } from "../../components";
+import { Header, HomeMain } from "../../components";
 export const Home = () => {
   return (
     <Fragment>
       <Header />
+      <HomeMain />
     </Fragment>
   );
 };
