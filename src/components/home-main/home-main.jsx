f90e1d7413693722216345b6561955591b3fe5db
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-18 16:53:41 +0500

    Main Sahifasini Home main ochildi

--- src/components/home-main/home-main.jsx
+++ src/components/home-main/home-main.jsx
@@ -2,7 +2,18 @@ import "./home-main.css";
 export const HomeMain = () => {
   return (
     <div className="HomeMain">
-      <h1>Main</h1>
+      <div className="HomeMain__img">
+        <div className="HomeMainImg__blur">
+          <h1 className="HomeMainImg__title">
+            Sizning Telefon nega haligacha eski?
+          </h1>
+          <p className="HomeMainImg__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ut
+            laboriosam, repellendus facilis quas fugit velit. Dignissimos porro
+            perferendis rem debitis libero eligendi inventore.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
