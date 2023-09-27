f1971ef57672ffbb1b13584f25a2a7a7f8acd4fd
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-27 17:32:08 +0500

    header sahifasi uchun jsx yarim tayyor xolatga keldi

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,22 @@
 import "./header.css";
+import { Link } from "react-router-dom";
 export const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <div className="Header">
+      <h1 className="Header__logo">
+        SUQ <span>MARKET</span>
+      </h1>
+      <nav className="HeaderNav">
+        <Link to="/" className="HeaderNav__link">
+          Asosiy sahifa
+        </Link>
+        <Link to="/" className="HeaderNav__link">
+          Maqolalar
+        </Link>
+        <Link to="/" className="HeaderNav__link">
+          Bog'lanish
+        </Link>
+      </nav>
+    </div>
+  );
 };
