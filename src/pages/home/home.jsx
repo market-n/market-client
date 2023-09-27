5e416252e43650379ab512c4154c5e02448b7b86
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-27 17:35:23 +0500

    Fragment olib tashlandi va o'rniga div bilan o'rab qo'yildi.

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,9 @@
-import { Fragment } from "react";
 import "./home.css";
 import { Header } from "../../components/header/header";
 export const Home = () => {
   return (
-    <Fragment>
+    <div className="Home">
       <Header />
-    </Fragment>
+    </div>
   );
 };
