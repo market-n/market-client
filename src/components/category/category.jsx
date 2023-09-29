475e309981d84e9142bab13c63aa87a9ee34c36c
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-29 19:37:33 +0500

    icon olib tashlandi.

--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -6,9 +6,7 @@ export const Category = () => {
         category <i className="bx bx-category icon"></i>
       </h1>
       <div className="CategoryTable">
-        <p className="CategoryTable__row">
-          <i class="bx bx-radio-circle-marked"></i>Texnikalar
-        </p>
+        <p className="CategoryTable__row">Texnikalar</p>
         <p className="CategoryTable__row">Dorilar</p>
         <p className="CategoryTable__row">Kompyuterlar</p>
         <p className="CategoryTable__row">Telefonlar</p>
