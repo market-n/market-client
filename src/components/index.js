96397a56a7e0b001bdb2ddff70281a749f4eb28d
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-30 15:52:55 +0500

    section name component tayyor

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export * from "./header/header.jsx";
 export * from "./home-component/home-component.jsx";
 export * from "./category/category.jsx";
+export * from "./section-name/section-name.jsx";
