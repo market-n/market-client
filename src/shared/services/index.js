932d7a30bebb2b7663260a0109fe5c7a1fe37f1b
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-30 08:00:46 +0500

    headers qismi tayyorlab olindi.

--- src/shared/services/index.js
+++ src/shared/services/index.js
@@ -2,3 +2,7 @@ import axios from "axios";
 import { api } from "../constant";
 export const axiosInstance = axios.create({
   baseURL: api,
+  headers: {
+    "Content-type": "application/json",
+  },
+});
